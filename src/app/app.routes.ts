6479cc52df2959afd630604d1e25a2354ca1a2da
Guilherme <willfkrause@outlook.com>
2025-02-03 18:17:52 -0300

    imported modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { WordCounterFieldComponent } from './word-counter-field/word-counter-field.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
 {
     path: '',
     component: WordCounterFieldComponent
 }
-];
+]
