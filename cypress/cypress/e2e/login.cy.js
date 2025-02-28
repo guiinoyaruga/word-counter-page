943396b9074a1281d10e8bdc03cf585d010ec41a
Guilherme <willfkrause@outlook.com>
2025-02-28 10:50:58 -0300

    add workflow

--- cypress/cypress/e2e/login.cy.js
+++ cypress/cypress/e2e/login.cy.js
@@ -1,6 +1,6 @@
 describe("Name of the group", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000");
+    cy.visit("http://www.google.com.br");
   });
 
   it("should behave...", () => {
